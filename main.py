15aadbde64a77d05f7526cf8fed6d6d464926824
samarth <samarplayz1337@gmail.com>
2024-11-19 22:29:56 +0530

    made some stuff dynamic

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 import jishaku
 
-TOKEN = ""  # Enter your discord client's token here
+TOKEN = input("Enter the token :")
 
 intents = discord.Intents.all()
 intents.presences = False
@@ -11,7 +11,7 @@ intents.presences = False
 bot = commands.AutoShardedBot(
     command_prefix="-",
     intents=intents,
-    owner_ids=["1207107876058046494", "1139950107995934863"]
+    owner_ids=["1139950107995934863"]
 )
 
 @bot.event
